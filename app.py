3253214c05b96153c2571fd4c433deaab10a9fa8
Anil Joseph <86954405+aniljoseph-ae@users.noreply.github.com>
2025-03-18 10:20:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import pickle
 import cv2
 from PIL import Image
 from ultralytics import YOLO
+#------------------------------------------------#
+import torch
+from ultralytics.nn.tasks import DetectionModel
+#------------------------------------------------#
 from langchain_community.vectorstores import FAISS
 # from langchain.docstore import InMemoryDocstore
 from langchain_community.docstore.in_memory import InMemoryDocstore
@@ -27,7 +31,12 @@ class EngineInspectionApp:
 
     def __init__(self):
         # Initialize YOLO model for defect detection
-        self.model_path = './yolov8n_model/best.pt'  # Path to YOLO model weights
+#---------------------------------------------------------------------------------------------#
+        # self.model_path = './yolov8n_model/best.pt'  # Path to YOLO model weights
+        torch.serialization.add_safe_globals([DetectionModel])
+        self.model = torch.load(self.model_path, map_location="cpu")
+        self.model.eval()
+#---------------------------------------------------------------------------------------------#
         if os.path.exists(self.model_path):
             self.model = YOLO(self.model_path)
         else:
